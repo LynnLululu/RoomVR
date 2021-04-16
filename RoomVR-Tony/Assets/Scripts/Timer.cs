f3cad96c6f37162e79d848a438c45234ae61f602
LynnLululu <luyiqun130@foxmail.com>
2021-04-16 02:33:41 -0500

    Time Count & Stop
    Change Count Down Time to Timer,
    When door open, timer stop

---
+++ RoomVR-Tony/Assets/Scripts/Timer.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Timer : MonoBehaviour
+{
+    public Text textObject;
+    private float startTime;
+    private float elapsedTime = 0f;
+    private TimeSpan timePlaying;
+    private bool finished = false;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        startTime = Time.time;
+        textObject.text = "Time: 00:00:00";
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (!finished)
+        {
+            elapsedTime += Time.deltaTime;
+            timePlaying = TimeSpan.FromSeconds(elapsedTime);
+            textObject.text = "Time: " + timePlaying.ToString("hh':'mm':'ss");
+        }
+    }
+
+    public void Finish()
+    {
+        finished = true;
+        textObject.color = Color.yellow;
+    }
+}
