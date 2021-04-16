487729fa5946542c9f1bf0a7c95a79ab3e093dc0
LynnLululu <luyiqun130@foxmail.com>
2021-04-16 02:08:45 -0500

    Add RoomVR_tony

---
+++ RoomVR-Tony/Assets/Scripts/Character.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Character : MonoBehaviour
+{
+    public float speed = 3.0F;
+    public float gravity = 10.0F;
+    CharacterController controller;
+
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        playerMovement();
+    }
+
+    //requirement: keyboards doesn't change the angle of the horizontal movement, 
+    //Instead, we use camera(rectile pointer) to enable player to move horizontally in any angle
+    void playerMovement()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        Vector3 direction = new Vector3(horizontal, 0, vertical);
+        Vector3 velocity = direction * speed;
+        //bind velocity with camera
+        velocity = Camera.main.transform.TransformDirection(velocity);
+        velocity.y = 0;
+        controller.Move(velocity * Time.deltaTime);
+    }
+}
